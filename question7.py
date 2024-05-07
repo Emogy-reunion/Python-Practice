d7037e5b4040cd3843121873f87bfca13da0d9b7
Emogy-reunion <mv7786986@gmail.com>
2024-05-07 08:06:20 +0300

    Added question7.py

--- question7.py
+++ question7.py
@@ -5,5 +5,5 @@ This program:
     Takes a list a makes a new list with only even elements
 """
 a = [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 10]
-new_list = [for x in a if x % 2 == 0]
+new_list = [x for x in a if x % 2 == 0]
 print(new_list)
