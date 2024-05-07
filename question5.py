be9e0a9b91428fe3c8a1e3f7b5a1269ef1a098cb
Emogy-reunion <mv7786986@gmail.com>
2024-05-07 07:16:03 +0300

    first commit

---
+++ question5.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""
+This program:
+    takes two lists
+    returns a list containing all elements that are common in both lists
+"""
+
+a = [1, 2, 3, 4, 5]
+b = [4, 7, 8, 9, 20, 5, 2, 5, 1]
+new_list = []
+for x in a:
+    if x in b:
+        new_list.append(x)
+
+print(new_list)
