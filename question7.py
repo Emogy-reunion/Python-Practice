7db9035834b252fc024535e801c3f1175e374a86
Emogy-reunion <mv7786986@gmail.com>
2024-05-07 07:58:18 +0300

    Added question7.py

---
+++ question7.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""
+This program:
+    List comprehension
+    Takes a list a makes a new list with only even elements
+"""
+a = [ 1, 4, 9, 16, 25, 36, 49, 64, 81, 10]
+new_list = [x % 2 == 0 for x in a]
