c363faf977df7ddb364c1555b315643df2b1fc71
Emogy-reunion <mv7786986@gmail.com>
2024-06-22 05:08:50 +0300

    feat: take user input and display it

---
+++ question10.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env  python3
+"""
+A program that prompts the user to enter their name
+It prints out the name and age
+"""
+
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hello {name}, you are {age} years old!")
